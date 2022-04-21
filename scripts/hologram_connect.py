6a48263d3f24fd2378534dcb4cf4a26f679212ef
Noah Young <noahpyoung@gmail.com>
2022-04-20 19:59:20 -0700

    added hard initial delay to hologram script

--- scripts/hologram_connect.py
+++ scripts/hologram_connect.py
@@ -2,9 +2,12 @@
 import time, sys
 from Hologram.CustomCloud import CustomCloud
 
+PRE_DELAY = 10
 RETRY_DELAY = 10
 
 print("Starting connect script")
+print("Waiting {:d} seconds".format(PRE_DELAY))
+time.sleep(PRE_DELAY)
 
 cloud = CustomCloud(None, network='cellular')
 cloud.network.disable_at_sockets_mode()
@@ -12,7 +15,7 @@ cloud.network.disable_at_sockets_mode()
 while cloud.network.getConnectionStatus() == 0:
     print("Cloud is not connected")
     res = cloud.network.connect()
-    print("response: {}".format(res))
+    print("Connection success: {}".format(res))
     if res:
         sys.exit(0)
     else:
